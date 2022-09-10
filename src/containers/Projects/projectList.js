f6e943367e60c7e2281b8ce38a710412c2b3b44e
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2022-09-09 21:23:08 -0300

    imagen PF corregidai

--- src/containers/Projects/projectList.js
+++ src/containers/Projects/projectList.js
@@ -4,14 +4,14 @@ const projectsList = [
       title: "Books Ecommerce",
       repo: "https://github.com/eknows-ecommerce/pf-front",
       link: "https://e-knows.herokuapp.com",
-      img: "https://media-exp1.licdn.com/dms/image/C4D2DAQHQfa9TTlaPqg/profile-treasury-image-shrink_800_800/0/1661895603682?e=1662638400&v=beta&t=oKbp8VnMiyrzHsWXZo_fGner9neFr1qm6gLg9cFKodo"
+      img: "https://i.ibb.co/MGp0D9B/1661895603682.jpg"
     }, 
     {
       id: "dogs",
       title: "Dogs PI",
       repo: "https://github.com/RichyMalicia/PI-Dogs",
       link: "https://dogs-api-bootcamp.vercel.app/", 
-      img: "https://i.ibb.co/MGp0D9B/1661895603682.jpg",
+      img: "https://i.ibb.co/YNCkDS8/dogs.png",
     },
     {
       id: "weather",
